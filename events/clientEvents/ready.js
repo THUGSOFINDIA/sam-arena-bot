50f42b4d43248c2b2a87b129a719a112c37ded04
thugsofindia <58879584+THUGSOFINDIA@users.noreply.github.com>
2020-07-19 20:17:45 +0530

    Update ready.js

--- events/clientEvents/ready.js
+++ events/clientEvents/ready.js
@@ -1,7 +1,7 @@
 module.exports = {
   name: "ready",
   async execute(client) {
-    client.user.setActivity(`Sam's Arena`, {
+    client.user.setActivity(`★D̲ꜱᴜ!!²²`, {
       type: "WATCHING",
     });
     console.log(`- Activated -`);
